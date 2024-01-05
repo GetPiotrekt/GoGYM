5f4a73e87e457f59989466fad3052001f1464b35
GetPiotrekt <92060850+GetPiotrekt@users.noreply.github.com>
2024-01-05 22:53:09 +0100

    Update Aplikacja.java
    Stały rozmiar, dobrze działające sceny

--- GoGYM/src/main/java/com/example/gogym/Aplikacja.java
+++ GoGYM/src/main/java/com/example/gogym/Aplikacja.java
@@ -7,20 +7,37 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Aplikacja extends Application {
+    private static Stage primaryStage;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        primaryStage.setWidth(360);
+        primaryStage.setHeight(640);
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("EKRAN_STARTOWY.fxml"));
         Parent root = loader.load();
 
         Kontroler controller = loader.getController();
-        controller.setPrimaryStage(primaryStage);
+        controller.setAplikacja(this);
 
         primaryStage.setTitle("Okno Główne");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static void zmienScene(String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Aplikacja.class.getResource(fxml));
+            Parent root = loader.load();
+
+            primaryStage.getScene().setRoot(root);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
